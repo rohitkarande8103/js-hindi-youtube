5d4a619d1500b9aca29e895ffa2daff3c72c73df
rohitkarande8103 <karanderohit925@gmail.com>
2025-07-17 11:36:40 +0000

    datatypes summary

---
+++ 01_basics/datatypes-summary.js
@@ -0,0 +1,26 @@
+// primitive
+
+//  7 types: String,Number, Boolean, null, undefined, symbol,bigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isloggedIn = false
+const outsideTemp = null
+let useremail;
+
+
+
+//  refrence (non primitive)
+// array , objects , functions
+
+const heros = ["shaktiman","naagraj"]
+let myObj = {
+    name: "rohit",
+    age:22,
+}
+
+// how to identyfy datatypes
+
+console.log(typeof outsideTemp);
+
