2fd066aae1200e274711a61097611605e7e67899
rohitkarande8103 <karanderohit925@gmail.com>
2025-07-17 10:56:22 +0000

    conversion of dataypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "rohit@google.com"
+var accountPassword = "12345"
+accountCity = "kolhapur"
+accountState;
+
+// accountId = 2 2nd id not allowed beacause of cons
+
+console.log(accountId);
+
+
+//  prefernot to use var because of issue in block scope and functional scope 
+
+console.table([accountEmail,accountId,accountPassword,accountCity,accountState])
---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,20 @@
+// console.log(typeof score);
+
+// when score = 33 it converts into = 33
+// '33abc' = NaN
+// true - 1 false = 0
+
+// let isloggedIn = "rohit"
+
+// let booleanIsloggedIn = boolean(IsloggedIn)
+// console.log(booleanIsloggedIn);
+
+// 1 = true: 0 = false
+//  "" = false
+//  "rohit" = true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+console.log(stringNumber);
+console.log(typeof stringNumber);
